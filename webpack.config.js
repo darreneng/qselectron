cfe55422e20c0b207b66292c20e5467b4f5ede42
Darren Eng <darrenle9@gmail.com>
2016-08-15 14:54:15 -0700

    .babelrc and webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+
+module.exports = {
+  //context: __dirname + '/src/',
+  target: 'electron',
+
+  entry: './src/entry.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/build'
+  },
+
+  resolve: {
+    modulesDirectories: ['node_modules', 'src'],
+    extension: ['', '.js', '.json']
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+
+      },
+      {
+        test: /\.(jpg|png|gif|eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+      }
+    ]
+  }
+};
